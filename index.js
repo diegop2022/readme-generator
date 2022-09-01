6b309b2741970f0fba44844e4072840fe30af70c
Diego <diegopena2022@gmail.com>
2022-08-31 17:29:02 -0700

    commit

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = []
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
+
